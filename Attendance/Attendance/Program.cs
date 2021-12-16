3f8d2bb18f5fcc656ae716524e8561a131ba5d4a
Your Name <akhilkumarchandragiri@gmail.com>
2021-12-16 14:06:15 +0530

    attendance

---
+++ Attendance/Attendance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Attendance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int emp_present = 1;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if ( empcheck == emp_present)
+            {
+                Console.WriteLine(" employee is present ");
+            }
+            else 
+            {
+                Console.WriteLine(" employee is absent ");
+            }
+        }
+    }
+}
