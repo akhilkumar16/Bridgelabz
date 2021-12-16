0613a61d0497f619f596fc0817f80741a1ff8a4b
Your Name <akhilkumarchandragiri@gmail.com>
2021-12-16 16:33:58 +0530

    switch case

---
+++ Switch_case/Switch_case/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Switch_case
+{
+    class Program
+    {
+        public const int parttime = 1;
+        public const int fulltime = 2;
+        public const int perhour = 20;
+        static void Main(string[] args)
+        {
+            int emphours = 0;
+            int wage = 0;
+            Random random = new Random();
+            int empcheck = random.Next(0, 3);
+            switch ( empcheck)
+            {
+                case parttime:
+                    emphours = 4;
+                    break;
+                case fulltime:
+                    emphours = 8;
+                    break;
+                default:
+                    emphours = 0;
+                    break;
+            }
+            wage = emphours * perhour;
+            Console.WriteLine(" employee wages : " + wage);
+        }
+    }
+}
